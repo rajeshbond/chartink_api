de1011ef4b9afd05779a6676835f0d548e6f10bf
rajeshbond <rajesh.sntsolutions@gmail.com>
2024-02-23 17:28:22 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ import asyncio
 from contextlib import asynccontextmanager
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Fetch data in the startup context
-    trasferDataToGoogleSheet()
-    yield
-    # No need to explicitly close resources here
-app = FastAPI(lifespan=lifespan)
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # Fetch data in the startup context
+#     trasferDataToGoogleSheet()
+#     yield
+#     # No need to explicitly close resources here
+# app = FastAPI(lifespan=lifespan)
 
-@app.get("/")
-async def root(lifespan):
-    print("Hello")
-    return {"message": "started"}
+# @app.get("/")
+# async def root(lifespan):
+#     print("Hello")
+#     return {"message": "started"}
 
-# app = FastAPI()
+app = FastAPI()
 
 
-# @app.get('/')
-# async def start():
-#     BackgroundTasks.add_task(trasferDataToGoogleSheet())
-#     return {"Message" : "Server Started"}
+@app.get('/')
+async def start():
+    BackgroundTasks.add_task(trasferDataToGoogleSheet())
+    return {"Message" : "Server Started"}
 
 
 
