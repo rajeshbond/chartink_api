875b0bbf040176c9404cd4ec35214f1c06340758
rajeshbond <rajesh.sntsolutions@gmail.com>
2024-02-24 12:53:12 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the FastAPI application using Uvicorn server
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=4)
 
 
 
